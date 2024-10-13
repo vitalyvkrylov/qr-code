7c15b3975e3305c292a0cf705c51199237efc902
Flet-IRNS <vitaly-krylov@yandex.ru>
2024-10-13 16:12:25 +0300

    delete files

---
+++ QR-code-with-logo.py
@@ -0,0 +1,7 @@
+from MyQR import myqr as mq
+
+mq.run(words='https://ege-drive.ru',
+       version=6,
+       picture='logo/logo.png',
+       colorized=True,
+       save_name='img-with-logo/ege-drive.png')
--- Qr-code-from-image.py
+++ Qr-code-from-image.py
@@ -3,24 +3,27 @@ from PIL import Image
 import base64
 
 # 1. Чтение изображения и преобразование в байты
-image_path = 'img.png'
+image_path = 'foto/img.png'  # Путь к вашему изображению
 with open(image_path, "rb") as image_file:
-    # Преобразуем изображение в байтовую строку и закодируем в base64
+    # Преобразуем изображение в байтовую строку и кодируем в base64
     image_bytes = image_file.read()
     image_base64 = base64.b64encode(image_bytes).decode('utf-8')
 
 # 2. Генерация QR-кода из закодированных данных
 qr = qrcode.QRCode(
-    version=1,
+    version=1,  # Версия определяет размер QR-кода
     error_correction=qrcode.constants.ERROR_CORRECT_H,  # Высокий уровень коррекции ошибок
-    box_size=10,
-    border=4
+    box_size=10,  # Размер ячеек QR-кода
+    border=4  # Граница вокруг QR-кода
 )
 qr.add_data(image_base64)
-qr.make(fit=False)
+qr.make(fit=True)
 
-# 3. Создание изображения QR-кода и сохранение
+# 3. Создание изображения QR-кода
 qr_image = qr.make_image(fill='black', back_color='white')
-qr_image.save('qr_from_image_data.png')
 
-print("QR-код с данными изображения успешно создан!")
+# 4. Сохранение изображения QR-кода
+output_path = 'qr_from_image.png'  # Укажите путь для сохранения QR-кода
+qr_image.save(output_path)
+
+print(f"QR-код с данными изображения успешно создан и сохранён как {output_path}!")
