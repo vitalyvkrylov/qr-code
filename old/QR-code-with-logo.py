7e05f73ea72766c37d1c903d8bec1af420953b1a
Flet-IRNS <vitaly-krylov@yandex.ru>
2024-11-03 12:47:57 +0300

    Refactor some files

--- old/QR-code-with-logo.py
+++
@@ -1,7 +0,0 @@
-from MyQR import myqr as mq
-
-mq.run(words='https://ege-drive.ru',
-       version=6,
-       picture='logo/logo.png',
-       colorized=True,
-       save_name='img-with-logo-with-logo/ege-drive.png')
